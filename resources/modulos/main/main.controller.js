15eedadb4897b510e2735bbbf2215fc5775d9ce1
Tarcísio Deschamps Silva <tarcisio.d.silva@accenture.com>
2017-05-18 13:24:50 -0300

    menu mobile

--- resources/modulos/main/main.controller.js
+++ resources/modulos/main/main.controller.js
@@ -1,5 +1,20 @@
-app.controller('MainController', function ($scope) {
+app.controller('MainController', function ($scope, $mdSidenav) {
   var vm = $scope;
 
   vm.title = 'Lista de contratos';
+  var originatorEv;
+
+  vm.toggleLeft = buildToggler('left');
+  vm.toggleRight = buildToggler('right');
+
+  function buildToggler(componentId) {
+    return function () {
+      $mdSidenav(componentId).toggle();
+    };
+  }
+
+  vm.openMenu = function ($mdMenu, ev) {
+    originatorEv = ev;
+    $mdMenu.open(ev);
+  };
 });
