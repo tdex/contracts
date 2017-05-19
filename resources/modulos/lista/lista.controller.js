6bc3b171e3d5c24684ebe2f5a6a4f9bb4d784cfa
Tarcisio Deschamps Silva <tardchamps94@gmail.com>
2017-05-19 20:47:06 -0300

    Excluir e pesquisa no menu

--- resources/modulos/lista/lista.controller.js
+++ resources/modulos/lista/lista.controller.js
@@ -1,10 +1,7 @@
 app.controller('ListaController', function ($scope, $http, ListaFactory, $mdDialog) {
   var vm = $scope;
 
-  vm.nome = 'tarcisio.d.silva';
   vm.lista = {};
-  vm.todosSelecionados = false;
-  // vm.filtroPesquisa = 'aprovado';
 
   vm.getListaContratos = function () {
     $http.get('resources/data_files/JSON.json').then(function (res) {
@@ -15,10 +12,19 @@ app.controller('ListaController', function ($scope, $http, ListaFactory, $mdDial
   vm.selecionaTodos = function () {
     if (vm.lista) {
       vm.lista.forEach(function (ct) {
-        ct.selecionado = vm.todosSelecionados;
-      }).then(function () {
-        vm.todosSelecionados = !vm.todosSelecionados;
+        ct.selecionado = !angular.isUndefined(ct.selecionado) ? vm.todosSelecionados : true;
       });
+      vm.todosSelecionados = !vm.todosSelecionados;
+    }
+  };
+
+  vm.excluirContratosSelecionados = function () {
+    if (vm.lista) {
+      for (var tamanhoLista = vm.lista.length, ct = tamanhoLista - 1; ct >= 0; ct--) {
+        if (vm.lista[ct].selecionado) {
+          vm.lista.splice(ct, 1);
+        }
+      }
     }
   };
 
@@ -32,8 +38,8 @@ app.controller('ListaController', function ($scope, $http, ListaFactory, $mdDial
       templateUrl: 'resources/modulos/contrato/contrato.modal.template.html',
       parent: angular.element(document.body),
       targetEvent: ev,
-      clickOutsideToClose: true,
-      fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
+      clickOutsideToClose: false,
+      fullscreen: true//vm.customFullscreen // Only for -xs, -sm breakpoints.
     })
       .then(function (answer) {
         vm.status = 'You said the information was "' + answer + '".';
--- resources/modulos/menu/menu.directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-app.directive('menu', function () {
-  return {
-    templateUrl: 'resources/modulos/menu/menu.template.html'
-  };
-});
-
